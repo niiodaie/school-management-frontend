1e57fcc73479ce79f888fd8613ce9516bbd7b2b1
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-16 17:46:51 -0500

    Create demoCredentials.js

---
+++ src/lib/demoCredentials.js
@@ -0,0 +1,22 @@
+// src/lib/demoCredentials.js
+
+const demoCredentials = {
+  admin: {
+    email: 'admin@greenwood.edu',
+    password: 'admin123'
+  },
+  teacher: {
+    email: 'teacher@greenwood.edu',
+    password: 'teacher123'
+  },
+  parent: {
+    email: 'parent@greenwood.edu',
+    password: 'parent123'
+  },
+  student: {
+    email: 'student@greenwood.edu',
+    password: 'student123'
+  }
+};
+
+export default demoCredentials;
